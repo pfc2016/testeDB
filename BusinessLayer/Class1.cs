716eb1e07827acce49478abcf43f6366fe71b1ee
Márcia <marcia_95_cost@hotmail.com>
2016-05-26 10:56:54 +0100

    1st Commit

--- BusinessLayer/Class1.cs
+++ BusinessLayer/Class1.cs
@@ -4,9 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BusinessLayer
-{
-    public class Class1
-    {
+namespace BusinessLayer {
+    public class Class1 {
     }
 }
--- DataLayer/Class1.cs
+++ DataLayer/Class1.cs
@@ -4,9 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DataLayer
-{
-    public class Class1
-    {
+namespace DataLayer {
+    public class Class1 {
     }
 }
