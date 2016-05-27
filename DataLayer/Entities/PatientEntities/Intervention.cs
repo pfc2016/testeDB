9eb5353019a3a18269c737ead683d6d0353b2cd3
esw2015 <marcia_95_cost@hotmail.com>
2016-05-27 20:33:20 +0100

    s

--- DataLayer/Entities/PatientEntities/Intervention.cs
+++ DataLayer/Entities/PatientEntities/Intervention.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.ComponentModel.DataAnnotations;
 namespace DataLayer.Entities.PatientEntities {
     public class Intervention {
-
+        [Key]
         public int Intervention_id {
             get; set;
         }
