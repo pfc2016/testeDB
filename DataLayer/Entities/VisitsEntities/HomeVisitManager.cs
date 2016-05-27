cb99ab46707dc35dea33cace6f3edd9b8852e6b0
esw2015 <marcia_95_cost@hotmail.com>
2016-05-27 20:29:25 +0100

    x

--- DataLayer/Entities/VisitsEntities/HomeVisitManager.cs
+++ DataLayer/Entities/VisitsEntities/HomeVisitManager.cs
@@ -12,6 +12,18 @@ namespace DataLayer.Entities.Visits {
         public int HomeVisitManager_id {
             get; set;
         }
+        public  HomeVisit home_Visit
+        {
+            get; set;
+        }
+        /// <summary>
+        /// um visita pode ter mais de um staff
+        /// </summary>
+        public List<Staff> staff_homeVisit
+        {
+            get; set;
+        }
+
 
     }
 }
