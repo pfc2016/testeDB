fb160c9227e981e55a9eb260c307e4e74623ad55
esw2015 <marcia_95_cost@hotmail.com>
2016-05-27 20:34:42 +0100

    s

--- DataLayer/Entities/PatientEntities/NextOfKinManager.cs
+++ DataLayer/Entities/PatientEntities/NextOfKinManager.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.ComponentModel.DataAnnotations;
 namespace DataLayer.Entities.PatientEntities {
     public class NextOfKinManager {
-
+        [Key]
         public int NextOfKinManager_id {
             get; set;
         }
