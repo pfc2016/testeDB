0a9e89eed01d99b5cbe084ce0ba04fce1a12679a
esw2015 <marcia_95_cost@hotmail.com>
2016-05-27 20:31:34 +0100

    s

--- DataLayer/Entities/MCDTEntities/LFT.cs
+++ DataLayer/Entities/MCDTEntities/LFT.cs
@@ -1,6 +1,6 @@
-﻿using System;
+﻿using System;using System.ComponentModel.DataAnnotations;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
