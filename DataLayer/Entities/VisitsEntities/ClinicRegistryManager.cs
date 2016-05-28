8557a04a74a6e10a6d1bb5f9f76e9bd0d7516707
esw2015 <marcia_95_cost@hotmail.com>
2016-05-29 00:08:34 +0100

    t

--- DataLayer/Entities/VisitsEntities/ClinicRegistryManager.cs
+++ DataLayer/Entities/VisitsEntities/ClinicRegistryManager.cs
@@ -1,4 +1,6 @@
-﻿using DataLayer.Entities.TreatmentEntities;
+﻿using DataLayer.Entities.DiagnosisEntities;
+using DataLayer.Entities.MCDT;
+using DataLayer.Entities.TreatmentEntities;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -20,6 +22,22 @@ namespace DataLayer.Entities.Visitas {
         public Patient Clinic_patient {
             get; set;
         }
+        /// <summary>
+        /// doente pode ter mais de um doenca
+        /// </summary>
+        public virtual List<MCDTManager> MCDT_manager
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// doente pode ter varios diagnosticos
+        /// </summary>
+        public List<Diagnosis> diagnosis
+        {
+            get; set;
+        }
+
 
         /// <summary>
         /// O doente pode ter vários medicamentos 
@@ -27,7 +45,9 @@ namespace DataLayer.Entities.Visitas {
         public virtual List<MedicationManager> Medication_manager {
             get; set;
         }
-
+        /// <summary>
+        /// o medico
+        /// </summary>
         public Staff Clinic_registry_doctor {
             get; set;
         }
@@ -35,10 +55,6 @@ namespace DataLayer.Entities.Visitas {
         public TherapyManager Therapy_manager {
             get; set;
         }
-        public List<VisitManager> visit_Manager
-        {
-            get; set;
-        }
-
+     
     }
 }
