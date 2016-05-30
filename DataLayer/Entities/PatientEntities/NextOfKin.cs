ba20d09c9d04611412659ed8b5c9b0157ea54214
esw2015 <marcia_95_cost@hotmail.com>
2016-05-30 13:25:28 +0100

    next of kin is a person

--- DataLayer/Entities/PatientEntities/NextOfKin.cs
+++ DataLayer/Entities/PatientEntities/NextOfKin.cs
@@ -4,13 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using DataLayer.Entities.UserEntities;
+
 namespace DataLayer.Entities.PatientEntities {
-    public class NextOfKin {
+    public class NextOfKin:Person {
         [Key]
         public int NextOfKin_id {
             get; set;
         }
-
+        [Key]
+        public NOFkinRelationShip nextOfKin_relationship
+        {
+            get; set;
+        }
         /// <summary>
         /// Navigation prop
         /// </summary>
