3224767765737c5cf109f8a8a8f4042f3b2b9905
esw2015 <marcia_95_cost@hotmail.com>
2016-05-31 19:29:26 +0100

    rectify model

--- DataLayer/Entities/VisitsEntities/FirstVisit.cs
+++ DataLayer/Entities/VisitsEntities/FirstVisit.cs
@@ -8,10 +8,7 @@ using System.Threading.Tasks;
 namespace DataLayer.Entities.Visitas {
     public class FirstVisit : Visit{
 
-        [Key]
-        public int FirstVisit_id {
-            get; set;
-        }
+       
         public ClinicRegistryManager ClinicRegistry_Manager
         {
             get; set;
